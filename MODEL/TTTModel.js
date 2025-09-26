f6d5f7568754e18457ff0240da7708c150fda658
Vktr22 <nds.viktoria@gmail.com>
2025-09-26 11:49:46 +0200

    model

---
+++ MODEL/TTTModel.js
@@ -0,0 +1,23 @@
+
+export default class TTTModel{
+    #elemLista = ["-","-","-","-","-","-","-","-","-"];
+    allapot = "X";
+    constructor(){
+        this.#elemLista = this.#elemLista;
+    }
+    getElemLista(){
+        return this.#elemLista;
+    }
+
+    getAllapot(){
+        return this.allapot;
+    }
+
+    getVegevanE(){
+
+    }
+
+    setAllapot(){
+        
+    }
+}
